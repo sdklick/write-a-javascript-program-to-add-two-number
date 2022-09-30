cf2db063e4cddab78febfaf0cbf2c739db987874
Sumanta Das <sumantad827@gmail.com>
2022-09-30 23:48:26 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//write a javascript program to add two number
+
+let num1=parseInt(prompt("please input 1st number : "))
+let num2=parseInt(prompt("please input 2nd number : "))
+
+let sum=num1+num2
+
+document.write(`${num1}+${num2}=${sum}`)
+
+//or you can print console
+
+console.log(`${num1}+${num2}=${sum}`);
+
+
+
